cf6ba0657e3e60c24715bdfbc99f2a5dee5f8f65
Sameer Dewan <sameer.dewan@itential.com>
2021-08-25 12:29:26 -0400

    hello_world.go

---
+++ 1-hello_world.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"text/template"
+)
+
+type Child struct {
+	Name string
+	Age  int
+}
+
+type Parent struct {
+	Name  string
+	Child string
+}
+
+func congradulateParents(parents []string, child string) {
+	for i := 0; i < len(parents); i++ {
+		msg := "Congradulations, {{.Name}} on the birth of your child, {{.Child}}!\n"
+		substitute := Parent{Name: parents[i], Child: child}
+		message, error := template.New("msg").Parse(msg)
+		error = message.Execute(os.Stdout, substitute)
+		if error != nil {
+			fmt.Println(error)
+		}
+	}
+}
+
+func growAYearOlder(name string, age int) {
+	msg := "Happy Birthday, {{.Name}}. You are now {{.Age}} year(s) old.\n"
+	substitute := Child{Name: name, Age: age}
+	message, error := template.New("msg").Parse(msg)
+	error = message.Execute(os.Stdout, substitute)
+	if error != nil {
+		fmt.Println(error)
+	}
+}
+
+func main() {
+	year := 27
+	parents := []string{"Thomas", "Sarah"}
+	child := "Thomas, Jr."
+	for i := 0; i <= year; i++ {
+		if i == 0 {
+			congradulateParents(parents, child)
+		} else {
+			growAYearOlder(child, i)
+		}
+	}
+}
