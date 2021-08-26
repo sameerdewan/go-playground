028db1a590b6bc21aef4fec8e1a4d3756e727ea8
Sameer Dewan <sameer.dewan@itential.com>
2021-08-26 07:12:52 +0000

    append to a slice

--- exercise6/exercise6.go
+++ exercise6/exercise6.go
@@ -18,9 +18,19 @@ func grabSlice(slice []interface{}, start uint, end uint) []interface{} {
 	return slice[start:end]
 }
 
+func testAppend() {
+	x := []int{1, 2, 3, 4, 5}
+	test := append(x, 6)
+	fmt.Printf("\nx:%v\ntest:%v", x, test) // x unmutated
+	y := []int{6, 7, 8}
+	x = append(x, y...)
+	fmt.Printf("\nx:%v", x) // x mutated
+}
+
 func Run() {
 	var slice []interface{} = generalizedSlice()
 	logSliceIndexValue(slice)
 	var slicedSlice []interface{} = grabSlice(slice, 0, 1)
 	fmt.Printf("\nslice:%v\nslicedSlice:%v\n", slice, slicedSlice) // test shows slicing a slice does not mutate the original slice
+	testAppend()
 }
