963a3580307f5481ed95635ec637bc8c98e0375b
Sameer Dewan <sameer.dewan@itential.com>
2021-08-26 01:59:11 +0000

    write out prompt

--- exercise1/exercise1.go
+++ exercise1/exercise1.go
@@ -1,7 +1,17 @@
 package exercise1
 
-import "fmt"
+/*
+	Exercise #1:
+	------------
+	Using the short declaration operator, ASSIGN these VALUES to VARIABLES with the IDENTIFIERS “x” and “y” and “z”
+	42
+	“James Bond”
+	true
+	Now print the values stored in those variables using
+	a single print statement
+	multiple print statements
+*/
 
 func Run() {
-	fmt.Println("This is from exercise 1!")
+
 }
