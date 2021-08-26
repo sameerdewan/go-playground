6eadf8c7a6908cd8133c7d74498ca6ead352b494
Sameer Dewan <sameer.dewan@itential.com>
2021-08-26 02:12:29 +0000

    bootstrap ex.2 with prompt

---
+++ exercise2/exercise2.go
@@ -0,0 +1,19 @@
+package exercise2
+
+/*
+	Exercise #2:
+	------------
+	Use var to DECLARE three VARIABLES.
+	The variables should have package level scope. Do not assign VALUES to the variables.
+
+	Use the following IDENTIFIERS for the variables and make sure the variables are of the following TYPE (meaning they can store VALUES of that TYPE).
+		a. identifier “x” type int
+		b. identifier “y” type string
+		c. identifier “z” type bool
+		d. print out the values for each identifier
+		e. The compiler assigned values to the variables. What are these values called?
+*/
+
+func Run() {
+
+}
--- practice_exercises.go
+++ practice_exercises.go
@@ -1,7 +1,11 @@
 package main
 
-import "github.com/sameerdewan/go-playground/exercise1"
+import (
+	"github.com/sameerdewan/go-playground/exercise1"
+	"github.com/sameerdewan/go-playground/exercise2"
+)
 
 func main() {
 	exercise1.Run()
+	exercise2.Run()
 }
