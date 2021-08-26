9552ae9b60bcc5f819f47b501283f0c04698f580
Sameer Dewan <sameer.dewan@itential.com>
2021-08-26 22:27:46 +0000

    using locks

---
+++ exercise13/exercise13.go
@@ -0,0 +1,29 @@
+package exercise13
+
+import (
+	"fmt"
+	"sync"
+)
+
+func Run() {
+	var counter int64
+
+	var wg sync.WaitGroup
+	wg.Add(100)
+
+	var mu sync.Mutex
+
+	for i := 0; i < 100; i++ {
+		go func() {
+			mu.Lock()
+			counter++
+			fmt.Println("a - Current count:\t\t", counter)
+			mu.Unlock()
+			wg.Done()
+		}()
+	}
+
+	fmt.Println("Reached the end of the program 1")
+
+	wg.Wait()
+}
