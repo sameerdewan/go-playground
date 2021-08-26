fd9277ce2c79a257d226213984816643749bf3a9
Sameer Dewan <sameer.dewan@itential.com>
2021-08-26 15:58:27 +0000

    pointer work

--- exercise9/exercise9.go
+++ exercise9/exercise9.go
@@ -4,9 +4,17 @@ import "fmt"
 
 func Run() {
 	a := 17
-	b := a // different pointers
+	b := a
 	fmt.Println(a)
 	fmt.Println(&a)
 	fmt.Println(b)
 	fmt.Println(&b)
+
+	c := &a
+	fmt.Println(c)
+	fmt.Println(*c)
+	fmt.Println(&a)
+
+	*c = 27
+	fmt.Println(a)
 }
