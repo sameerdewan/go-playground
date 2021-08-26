0d0e55f947e20191d0564bdb344b8008c7dd36d0
Sameer Dewan <sameer.dewan@itential.com>
2021-08-26 06:01:10 +0000

    work with iota

--- exercise5/exercise5.go
+++ exercise5/exercise5.go
@@ -2,6 +2,17 @@ package exercise5
 
 import "fmt"
 
+func printNumAsDecBinHex(number int) {
+	var intToFloat float64 = float64(number)
+	fmt.Printf("\ndecimal:%f\nbinary:%b\nhexidecimal:%x\n\n", intToFloat, number, number)
+}
+
+const (
+	example1 = iota
+	example2 = iota
+	example3 = iota
+)
+
 func Run() {
 	a := 4
 	fmt.Printf("a) %d\t\t%b\n", a, a)
@@ -20,4 +31,19 @@ func Run() {
 	fmt.Printf("f) %d\t\t%b\n", f, f)
 	g := a | f
 	fmt.Printf("g) %d\t\t%b\n", g, g)
+
+	printNumAsDecBinHex(3)
+
+	exampleStringLiteral := `
+		This string literal will retain formatting
+
+			ie: line skipping 
+
+
+		ie:          spacing,
+	`
+
+	fmt.Println(exampleStringLiteral)
+
+	fmt.Printf("\n\nexample1:%v\nexample2:%v\nexample3:%v\n", example1, example2, example3)
 }
