1f2f1c752f46e5b2f48732b2130c65668a213cbe
Sameer Dewan <sameer.dewan@itential.com>
2021-08-26 05:38:46 +0000

    playing w bitwise ex5

--- exercise5/exercise5.go
+++ exercise5/exercise5.go
@@ -3,9 +3,21 @@ package exercise5
 import "fmt"
 
 func Run() {
-	x := 4
-	fmt.Printf("%d\t\t%b", x, x)
+	a := 4
+	fmt.Printf("a) %d\t\t%b\n", a, a)
 
-	y := x << 1
-	fmt.Printf("%d\t\t%b", y, y)
+	b := a << 1
+	fmt.Printf("b) %d\t\t%b\n", b, b)
+
+	c := a | b
+	fmt.Printf("c) %d\t\t%b\n", c, c)
+
+	d := 8
+	e := a | d
+	fmt.Printf("e) %d\t\t%b\n", e, e)
+
+	f := 18
+	fmt.Printf("f) %d\t\t%b\n", f, f)
+	g := a | f
+	fmt.Printf("g) %d\t\t%b\n", g, g)
 }
