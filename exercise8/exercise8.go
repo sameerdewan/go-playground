7a3fe77b52369c40fc59cfc97d76fc95f6380ef7
Sameer Dewan <sameer.dewan@itential.com>
2021-08-26 14:10:03 +0000

    dogs example

--- exercise8/exercise8.go
+++ exercise8/exercise8.go
@@ -2,6 +2,12 @@ package exercise8
 
 import "fmt"
 
+type Dog string
+
+func (dog Dog) bark() {
+	fmt.Printf("Woof! I am a %v\n", dog)
+}
+
 func spreadParams(x ...int) { // "Variadic parameters"
 	for i := 0; i < len(x); i++ {
 		fmt.Println(x[i])
@@ -12,4 +18,8 @@ func Run() {
 	spreadParams(1, 2, 3, 4, 5)
 	spreadable := []int{6, 7, 8, 9}
 	spreadParams(spreadable...)
+	var goldenRetriever Dog = "Golden Retriever"
+	goldenRetriever.bark()
+	var shibaInu Dog = "Shiba Inu"
+	shibaInu.bark()
 }
